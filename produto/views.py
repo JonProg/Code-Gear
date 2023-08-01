d553b883b2d6ac3d6534ff7b1069544ef755b389
jon_prog <jonashenri.aquino@gmail.com>
2023-08-01 02:58:42 -0300

    started view AddCarrinho

--- produto/views.py
+++ produto/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import resolve_url, redirect, get_object_or_404
+from django.contrib import messages
 from django.views import View
 from . import models
 
@@ -15,7 +17,35 @@ class DetalheProduto(DetailView):
     slug_url_kwarg = 'slug'
 
 class AddCarrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            resolve_url('produto:lista')
+        )
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não existe'
+            )
+            return redirect(http_referer)
+        
+        variacao = get_object_or_404(models.Variacao, id=variacao_id)
+
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+
+        carrinho = self.request.session['carrinho']
+
+        if variacao_id in carrinho:
+            pass
+        else:
+            pass
+        
+
+
 
 class RemoveCarrinho(View):
     pass
